9b8ae6440be9a657a90779d09ae7852222e9858f
muneebimtiazz <muneebimtiaz6@gmail.com>
2025-07-06 09:55:31 +0500

    suspense loader fix

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,9 @@
 import './App.css'
-import {lazy, Suspense  } from 'react';
-
-import { ToastContainer} from 'react-toastify';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { Suspense, lazy } from 'react';
+import loader from './assets/images/Spinner-2.gif'
 import Layout from './components/Layout'
-import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+
 
 // lazy loading
 const Home=lazy(()=>import('./pages/Home'))
@@ -16,36 +16,30 @@ const Login=lazy(()=>import('./pages/Login'))
 const NotFound=lazy(()=>import('./pages/NotFound'))
 const Checkout=lazy(()=>import('./pages/Checkout'))
 
-const Loading = () => (
-  <div className="flex justify-center items-center h-screen">
-    <div className="animate-spin rounded-full h-12 w-12 border-t-2 border-b-2 border-blue-500"></div>
-  </div>
-);
+    const router=createBrowserRouter( 
+      [{
+      element:<Layout/>,
+      children:[
+      {path:'/',element:<Home/>},
+      {path:'/shop',element:<Shop/>},
+      {path:'/about',element:<About/>},
+      {path:'/contact',element:<Contact/>},
+      {path: '/product/:productId',element:<Product/> },
+      {path: '/cart',element:<Cart/>},
+      {path: '/login',element:<Login/>},
+      {path: '/checkout',element: <Checkout/>},
+      {path:'*',element: <NotFound/>}]
+      }]
+    )
 
-
-  const router=createBrowserRouter( 
-    [{
-    element:<Layout/>,
-    children:[
-    {path:'/',element: <Suspense fallback={<loading />}><Home/></Suspense> },
-    {path:'/shop',element:<Suspense fallback={<loading />}><Shop/></Suspense>},
-    {path:'/about',element:<Suspense fallback={<loading/>}> <About/></Suspense>},
-    {path:'/contact',element:<Suspense fallback={<loading/>}> <Contact/></Suspense>},
-    {path: '/product/:productId',element:<Suspense fallback={<loading/>}> <Product/></Suspense> },
-    {path: '/cart',element:<Suspense fallback={<loading/>}> <Cart/></Suspense> },
-    {path: '/login',element:<Suspense fallback={<loading/>}> <Login/></Suspense> },
-    {path: '/checkout',element:<Suspense fallback={<loading/>}> <Checkout/></Suspense> },
-    {path:'*',element:<Suspense fallback={<loading/>}> <NotFound/></Suspense>}]
-    }]
-  )
-
-function App() {
-  return (
-    <div>
-        <ToastContainer/>
-        <RouterProvider router={router}/>
-    </div>
-  
-  )
-}
-export default App
+  function App() {
+    return (
+      <div className='min-h-screen'>
+        <Suspense fallback={<div className="h-screen w-full flex justify-center items-center"><img className='w-20 h-20 object-contain' src={loader} alt="loading" /></div>}>
+          <RouterProvider router={router} />
+        </Suspense>
+      </div>
+    
+    )
+  }
+  export default App
